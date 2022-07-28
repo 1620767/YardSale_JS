7d3cc95ef5c50ca067e11edbab1dbcff6e6568e3
Luis Puse <puseluis@gmail.com>
2022-07-28 16:10:42 -0500

    Carrito con js

--- js/main.js
+++ js/main.js
@@ -2,14 +2,41 @@ const navEmail = document.querySelector('.nav-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const iconMenu = document.querySelector('.icon-menu') 
 const mobileMenu = document.querySelector('.mobile-menu') 
+const menuCarrito = document.querySelector('.nav-shopping-car')
+const asideCarrito = document.querySelector('.product-detail')
+
 
 navEmail.addEventListener('click', toggleMenu);
 iconMenu.addEventListener('click', toggleMobileMenu);
+menuCarrito.addEventListener('click', toggleCarrito);
 
 function toggleMenu() {
+    if (!asideCarrito.classList.contains('inactive')) {
+        asideCarrito.classList.add('inactive')
+    }
+
     desktopMenu.classList.toggle('inactive')
 }
 
 function toggleMobileMenu() {
+    if (!asideCarrito.classList.contains('inactive')) {
+        asideCarrito.classList.add('inactive')
+    }
+
     mobileMenu.classList.toggle('inactive')
+}
+
+
+function toggleCarrito() {
+
+    if (!mobileMenu.classList.contains('inactive')) {
+        mobileMenu.classList.add('inactive')
+    }
+
+    if (!desktopMenu.classList.contains('inactive')) {
+        desktopMenu.classList.add('inactive')
+    }
+
+    asideCarrito.classList.toggle('inactive')
+      
 }
