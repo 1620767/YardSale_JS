88a98b331f0e8c0f6f53b9d9fb4b8194773dc269
Luis Puse <puseluis@gmail.com>
2022-07-28 15:08:19 -0500

    Add menu-mobile

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 const navEmail = document.querySelector('.nav-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const iconMenu = document.querySelector('.icon-menu') 
+const mobileMenu = document.querySelector('.mobile-menu') 
 
 navEmail.addEventListener('click', toggleMenu);
+iconMenu.addEventListener('click', toggleMobileMenu);
 
 function toggleMenu() {
     desktopMenu.classList.toggle('inactive')
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive')
 }
